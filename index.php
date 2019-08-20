a223245b4d2401b654ab80cfbdf9f3b49faac935
Elneris <elneris.dang@gmail.com>
2019-08-20 15:48:06 +0200

    refactoring

--- index.php
+++ index.php
@@ -3,20 +3,6 @@
 require 'Rectangle.php';
 require 'Square.php';
 
-/*$rectangle = new Rectangle();
-$rectangle->width = 200;
-$rectangle->height = 150;
-$rectangle->color = "blue";
-
-var_dump($rectangle);*/
-
-/*$rectangle1 = new Rectangle();
-$rectangle1->setWidth(-200);
-$rectangle1->setHeight(150);
-$rectangle1->setColor('blue');
-
-var_dump($rectangle1);*/
-
 $rectangle2 = new Rectangle(-200, 150);
 
 echo($rectangle2);
